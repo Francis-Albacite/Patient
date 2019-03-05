7237183691ca926f6d69b88a0a3deebd1236b542
francisleif <alfblademaster@gmail.com>
2019-03-05 14:29:03 +0800

    Update ViewPatient.aspx.cs

--- ViewPatient.aspx.cs
+++ ViewPatient.aspx.cs
@@ -11,12 +11,18 @@ public partial class ViewPatient : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        XElement _xml = XElement.Load("D:/patient.xml");
-        IEnumerable<XElement> childElements =
-            from element in _xml.Elements()
-            select element;
-        foreach (XElement el in childElements)
-            Console.WriteLine("FirstName: " + el.Name);
-        
+        XmlDocument xml = new XmlDocument();
+        xml.LoadXml("D:/patient.xml"); 
+
+        XmlNodeList xnList = xml.SelectNodes("/Patients/Patient");
+        foreach (XmlNode xn in xnList)
+        {
+            string firstName = xn["FirstName"].InnerText;
+            string lastName = xn["LastName"].InnerText;
+            Console.WriteLine("Name: {0} {1}", firstName, lastName);
+        }
+
+
+
     }
 }
